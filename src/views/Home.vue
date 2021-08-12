515f63bdbffa477eaccc10dbe5b69228cbf2b181
Paige <paigechoi0701@gmail.com>
2021-08-13 00:08:04 +0900

    add chartjs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,54 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="small">
+      <line-chart :chart-data="datacollection"></line-chart>
+      <button @click="fillData()">Randomize</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import LineChart from "@/components/charts/LineChart.js";
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    LineChart
+  },
+  data() {
+    return {
+      datacollection: null
+    }
+  },
+  mounted() {
+    this.fillData()
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }, {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }
+        ]
+      }
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50-5 + 1)) + 5
+    }
   }
 }
 </script>
+<style>
+.small {
+  max-width: 600px;
+  margin:  150px auto;
+}
+</style>
