2ac89555bd07c1c2ca8709626f20637e45ef7729
Paige <paigechoi0701@gmail.com>
2021-08-16 15:20:50 +0900

    add settings (eslint)

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  "extends": [
+    "plugin:vue/recommended",
+  ],
+  "plugins": ['vuetify'],
+  "rules": {
+    'vuetify/no-deprecated-classes': 'error'
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
   <v-app>
     <v-app-bar
-        app
-        color="danger"
-        dark
+      app
+      color="danger"
+      dark
     >
       <div class="d-flex align-center">
         <router-link to="/">
-        <v-img
+          <v-img
             alt="Vuetify Logo"
             class="shrink mr-2"
             contain
             src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
             transition="scale-transition"
             width="30"
-        />
+          />
         </router-link>
       </div>
 
-      <router-link to="/about">About</router-link> |
-      <router-link to="/profile">Profile</router-link>
-      <v-spacer></v-spacer>
+      <router-link to="/about">
+        About
+      </router-link> |
+      <router-link to="/profile">
+        Profile
+      </router-link>
+      <v-spacer />
 
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
@@ -34,13 +38,59 @@
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
+    <v-navigation-drawer app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
+      <v-divider />
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="item.to"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: 'Home', icon: 'mdi-view-dashboard', to:"/" },
+        { title: 'Profile', icon: 'mdi-image', to:"/profile" },
+        { title: 'About', icon: 'mdi-help-box', to:"/about" },
+      ],
+    }
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
