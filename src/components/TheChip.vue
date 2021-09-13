cad89ce21d6bd3c42fc06728116204ff966bc6a5
Paige <paigechoi0701@gmail.com>
2021-09-13 18:22:08 +0900

    create chip component

---
+++ src/components/TheChip.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-chip
+    v-if="isShow"
+    class="ma-2"
+    close
+    :color="randomColour"
+    outlined
+    @click="$emit('delete')"
+  >
+    {{ name }}
+  </v-chip>
+</template>
+<script>
+export default {
+  name: 'CommonChip',
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isShow: true
+    }
+  },
+  computed: {
+    randomColour() {
+      return '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container fluid>
+    <v-card>
+      <template v-for="item in selectedWorkouts">
+        <common-chip
+          :key="item._index"
+          :name="item"
+          @delete="onDeleteChip"
+        />
+      </template>
+    </v-card>
+  </v-container>
+</template>
+<script>
+import CommonChip from "@/components/TheChip";
+export default {
+  name: 'TheHome',
+  components: {
+    CommonChip
+  },
+  data() {
+    return {
+      selectedWorkouts: ['ashtanga', 'leg', 'arm']
+    }
+  },
+  methods: {
+    onDeleteChip() {
+      console.info('on delete chip')
+    }
+  }
+}
+</script>
