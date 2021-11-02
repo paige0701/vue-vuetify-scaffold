18bdeca68e140e571ba5404b572969e2ed369f79
Paige <paigechoi0701@gmail.com>
2021-11-02 15:19:47 +0900

    fix signin btn css

--- src/views/authentication/SignInMethods.vue
+++ src/views/authentication/SignInMethods.vue
@@ -1,54 +1,73 @@
 <template>
   <v-container class="fill-height">
     <v-row justify="center">
-      <v-col cols="auto">
-        <router-link to="/" />
-        <v-card
-          class="text-center px-12 py-15"
-          width="460"
-        >
-          <v-btn
-            block
-            class="mt-10"
-            color="primary"
-            rounded
-            x-large
-            @click="login('google')"
+      <v-col
+        cols="auto"
+        class="py-15"
+      >
+        <v-card-text class="text-h2">
+          Login
+        </v-card-text>
+        <v-card>
+          <a
+            class="mt-3"
+            @click="login('kakao')"
           >
-            Login with google
-          </v-btn>
-          <a id="custom-login-btn" @click="login('kakao')">
             <img
               src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
-              width="222"
-            />
+              width="250"
+            >
           </a>
+          <GoogleLogin
+            class="mt-3"
+            :params="params"
+            :render-params="renderParams"
+            :on-success="onSuccess"
+            :on-failure="onFailure"
+          />
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
+import GoogleLogin from 'vue-google-login'
+
 export default {
+  components: {GoogleLogin},
   data: () => {
     return {
       email: '',
       password: '',
       auth: '',
       provider: '',
+      params: {
+        client_id: process.env.VUE_APP_ENV_GOOGLE_CLIENT_ID
+      },
+      renderParams: {
+        width: 250,
+        height: 50,
+        longtitle: true
+      }
     }
   },
   methods: {
+    onSuccess(googleUser) {
+      console.info(`success -- `, googleUser)
+    },
+    onFailure() {
+
+    },
     login(method) {
       if (method === 'kakao') {
         Kakao.Auth.login({
           success: async (authObj) => {
-            const { data } = await this.$api.workout.loginWithKakao(authObj)
+            const {data} = await this.$api.workout.loginWithKakao(authObj)
             this.$cookies.set('access_token', data.access_token)
             this.$cookies.set('refresh_token', data.refresh_token)
             await this.$router.push({name: 'Home'})
           },
-          fail: function(err) {
+          fail: function (err) {
             alert(`kakako login failed, ${err}`)
           },
         })
