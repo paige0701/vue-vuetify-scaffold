6719fd0b1749f246e0235353fccfcc3927151da2
Paige <paigechoi0701@gmail.com>
2021-11-03 18:30:14 +0900

    add refresh token Login when access token is expired

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import NAVER from '@/api/naver'
 import NYT from '@/api/nyt'
 import WORKOUT from '@/api/workout'
 import axios from 'axios'
-
+import router from './router'
 const naverInstance = axios.create({})
 
 naverInstance.interceptors.request.use((config) => {
@@ -17,6 +17,55 @@ const nytInstance = axios.create({
 })
 
 const baseAxiosInstance = axios.create({})
+baseAxiosInstance.interceptors.response.use( (response) => {
+  // Return a successful response back to the calling service
+  return response;
+}, (error) => {
+  // Return any error which is not due to authentication back to the calling service
+  if (error.response.status !== 401) {
+    return new Promise((resolve, reject) => {
+      reject(error);
+    });
+  }
+
+  // Logout user if token refresh didn't work or user is disabled
+  if (error.config.url === '/api/v1/authentication/token/refresh/') {
+
+    Vue.$cookies.set('access_token', '')
+    Vue.$cookies.set('refresh_token', '')
+    router.push({name: 'SignInMethods'})
+
+    return new Promise((resolve, reject) => {
+      reject(error);
+    });
+  }
+
+  return baseAxiosInstance.post(`/api/v1/authentication/token/refresh/`, {
+    refresh: Vue.$cookies.get('refresh_token')
+  })
+    .then(({data}) => {
+      console.info(`token ? ${data.access}`)
+      console.info(`error ? ${error}`)
+
+      // New request with new token
+      const config = error.config;
+      config.headers['Authorization'] = `Bearer ${data.access}`;
+
+      return new Promise((resolve, reject) => {
+        axios.request(config).then(response => {
+          resolve(response);
+        }).catch((error) => {
+          reject(error);
+        })
+      });
+
+    })
+    .catch((error) => {
+      Promise.reject(error);
+    });
+});
+
+
 baseAxiosInstance.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${Vue.$cookies.get('access_token')}`
   return config
