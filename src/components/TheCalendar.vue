824fd8ada65d64a1a23e960b6622e11340bcfe2b
Paige <paigechoi0701@gmail.com>
2021-11-09 13:29:45 +0900

    Refactoring TheCalendarView, Remove fix button in calendar detail

--- src/components/TheCalendar.vue
+++ src/components/TheCalendar.vue
@@ -68,16 +68,15 @@
               :color="selectedEvent.color"
               dark
             >
+              <v-toolbar-title v-html="selectedEvent.name" />
+              <v-spacer />
+              <v-spacer />
               <v-btn
                 icon
                 @click="goToEdit"
               >
                 <v-icon>mdi-pencil</v-icon>
               </v-btn>
-              <v-toolbar-title v-html="selectedEvent.name" />
-              <v-spacer />
-              <v-spacer />
-              <the-dialog :date="selectedEventDate" />
             </v-toolbar>
             <v-card-text>
               <span v-html="selectedEvent.details" />
@@ -95,78 +94,30 @@
         </v-menu>
       </v-sheet>
     </v-col>
-    <v-dialog
-      v-model="dialog"
-      fullscreen
-      hide-overlay
-      transition="dialog-bottom-transition"
-    >
-      <v-card>
-        <v-toolbar
-          dark
-          color="primary"
-        >
-          <v-btn
-            icon
-            dark
-            @click="dialog = false"
-          >
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-          <v-toolbar-title>Move</v-toolbar-title>
-          <v-spacer />
-        </v-toolbar>
-        <v-list
-          three-line
-          subheader
-        >
-          <v-subheader justify="center">
-            DO you want to add new workout to {{ selectedEventDate }}?
-          </v-subheader>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn
-              color="green darken-1"
-              text
-              @click="dialog = false"
-            >
-              No
-            </v-btn>
-            <v-btn
-              color="red darken-1"
-              text
-              @click="goToEdit"
-            >
-              Yes
-            </v-btn>
-          </v-card-actions>
-        </v-list>
-      </v-card>
-    </v-dialog>
+    <the-confirm-dialog
+      v-model="confirmDialog"
+      :title="'Move'"
+      :description="`DO you want to add new workout to ${selectedEventDate}`"
+      @close="dialogAction($event)"
+    />
   </v-row>
 </template>
 <script>
-import TheDialog from "@/components/TheDialog";
 import dayjs from "dayjs";
-import {map, range, reduce} from "lodash";
+import {range, reduce} from "lodash";
+import TheConfirmDialog from "@/components/dialogs/TheConfirmDialog";
 export default {
-  components: {TheDialog},
+  components: {TheConfirmDialog},
   data: () => ({
     selectedEventDate: '',
     focus: '',
     type: 'month',
-    typeToLabel: {
-      month: 'Month',
-      week: 'Week',
-      day: 'Day',
-    },
     dialog: false,
+    confirmDialog: false,
     selectedEvent: {},
     selectedElement: null,
     selectedOpen: false,
     events: [],
-    colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-    names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
   }),
   async mounted() {
     this.$refs.calendar.checkChange()
@@ -180,6 +131,53 @@ export default {
         return `<ul>${items}</ul>`
       }
       return ''
+    },
+    goToEdit() {
+      this.$router.push({name: 'Home', params: {id: this.selectedEventDate}})
+    },
+    getEventColor(event) {
+      return event.color
+    },
+    setToday() {
+      this.focus = ''
+    },
+    prev() {
+      this.$refs.calendar.prev()
+    },
+    next() {
+      this.$refs.calendar.next()
+    },
+    showEvent({nativeEvent, event, date}) {
+
+      // 오늘보다 미래는 등록안됨
+      if (dayjs(date) > dayjs() ) {
+        return
+      }
+      if (!event) {
+        this.selectedEventDate = date
+        this.confirmDialog = true
+        // dialog 띄우고 새로운 event 등록할거냐고 묻고 이동한다.
+        return
+      }
+      this.selectedEventDate = event.start
+      const open = () => {
+        this.selectedEvent = event
+        this.selectedElement = nativeEvent.target
+        requestAnimationFrame(() => requestAnimationFrame(() => this.selectedOpen = true))
+      }
+
+      if (this.selectedOpen) {
+        this.selectedOpen = false
+        requestAnimationFrame(() => requestAnimationFrame(() => open()))
+      } else {
+        open()
+      }
+
+      nativeEvent.stopPropagation()
+    },
+    async updateRange({start}) {
+      let today = start.date.split('-')
+      this.events = await this.getMonthlyRecord(`${today[0]}-${today[1]}`)
     },
     async getMonthlyRecord(yearMonth) {
       const { data: records } = await this.$api.workout.getMonthlyRecord(yearMonth)
@@ -212,48 +210,11 @@ export default {
       })
       return results
     },
-    goToEdit() {
-      this.$router.push({name: 'Home', params: {id: this.selectedEventDate}})
-    },
-    getEventColor(event) {
-      return event.color
-    },
-    setToday() {
-      this.focus = ''
-    },
-    prev() {
-      this.$refs.calendar.prev()
-    },
-    next() {
-      this.$refs.calendar.next()
-    },
-    showEvent({nativeEvent, event, date}) {
-      if (!event) {
-        this.selectedEventDate = date
-        this.dialog = true
-        // dialog 띄우고 새로운 event 등록할거냐고 묻고 이동한다.
-        return
+    async dialogAction(data) {
+      this.confirmDialog = false
+      if (data) {
+        this.goToEdit()
       }
-      console.info(event)
-      this.selectedEventDate = event.start
-      const open = () => {
-        this.selectedEvent = event
-        this.selectedElement = nativeEvent.target
-        requestAnimationFrame(() => requestAnimationFrame(() => this.selectedOpen = true))
-      }
-
-      if (this.selectedOpen) {
-        this.selectedOpen = false
-        requestAnimationFrame(() => requestAnimationFrame(() => open()))
-      } else {
-        open()
-      }
-
-      nativeEvent.stopPropagation()
-    },
-    async updateRange({start}) {
-      let today = start.date.split('-')
-      this.events = await this.getMonthlyRecord(`${today[0]}-${today[1]}`)
     },
   },
 }
